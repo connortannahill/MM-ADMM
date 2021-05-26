9068741692e8124ca44643e10bfea6fea36c6b27
HEC2018 <hectorweica@gmail.com>
2021-05-26 18:11:44 -0400

    Sligt Modification on BlockgradC

--- src/AdaptationFunctional.h
+++ src/AdaptationFunctional.h
@@ -39,6 +39,10 @@ public:
                 Eigen::Vector<double, D*(D+1)> &xi,
                 Eigen::Vector<double, D*(D+1)> &grad,
                 MeshInterpolator<D> &interp);
+    double blockGradC(int zId, Eigen::Vector<double, D*(D+1)> &z,
+                Eigen::Vector<double, D*(D+1)> &xi,
+                Eigen::Vector<double, D*(D+1)> &grad,
+                MeshInterpolator<D> &interp);
     virtual ~AdaptationFunctional();
 private:
 };
